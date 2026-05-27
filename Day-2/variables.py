eaaba90e155804b85d5feb1071b49a68549ad899
aiswaryaaragala-dev <aiswaryaaragala@gmail.com>
2026-05-27 17:49:23 +0530

    Add files via upload

---
+++ Day-2/variables.py
@@ -0,0 +1,55 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================================== RESTART: C:/Users/aiswa/OneDrive/Desktop/python-course-work/Day-2/keyword.py =====================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+myval=10
+MYval=10
+MyVal=10
+myval1=10
+myval@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+my_val=10
+my val=10
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+val=10
+val=20
+val
+20
+val
+20
+Val=10
+Val
+10
+a=b=c=10
+a
+10
+b
+10
+c
+10
+a,b,c=10,20,30
+a
+10
+b
+20
+c
+30
+a=10
+b=20
+a=b=b=a
+a
+10
+a,b=b,a
+a
+10
+b
+10
+a=10
+b=20
+a,b=b,a
+b
+10
+a
+20
